5e25271d7113b767ad47a33319e7d001a07c3e2e
宁卿 <ningqing@ningqingdeMacBook-Pro.local>
2025-11-16 22:41:17 +0800

    更新于: 2025-11-16 22:38:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/10/hello-world/","2025/10/10/测试文章/","2025/10/20/优泌可/"];function toRandomPost(){
+var posts=["2025/10/10/hello-world/","2025/10/10/测试文章/","2025/10/20/CARBESIA注射液/","2025/10/22/测试2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
