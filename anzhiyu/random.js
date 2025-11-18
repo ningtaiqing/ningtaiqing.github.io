8c62af22bba47e933d412bac9f543d3ad08b17de
宁卿 <ningqing@ningqingdeMacBook-Pro.local>
2025-11-18 20:17:06 +0800

    更新于: 2025-11-18 20:15:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/10/hello-world/","2025/10/10/测试文章/","2025/10/20/CARBESIA注射液/","2025/10/22/测试2/"];function toRandomPost(){
+var posts=["2025/10/10/hello-world/","2025/10/10/测试文章/","2025/10/20/CARBESIA注射液/","2025/10/22/测试2/","2025/11/18/职称申报材料和流程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
