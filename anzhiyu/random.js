c320751c5daa33bf97245f875a68571e88b18b2d
浦奥 <puao@puaodeMacBook-Air.local>
2025-11-27 23:19:51 +0800

    更新于: 2025-11-27 23:19:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/18/职称申报材料和流程/","2025/10/22/RadiAnt DICOM Viewer：可以打开原始格式(DICOM)影像资料的免费软件/","2025/11/27/犬猫液体给予速率/"];function toRandomPost(){
+var posts=["2025/11/18/职称申报材料和流程/","2025/10/22/RadiAnt DICOM Viewer：可以打开原始格式(DICOM)影像资料的免费软件/","2025/11/27/犬猫液体给予速率/","2025/11/27/血常规、生化各项指标代表的意义/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
